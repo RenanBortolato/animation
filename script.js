87bc84b28ead5af815eb846ed5554a5dc6361593
RenanBortolato <75533194+RenanBortolato@users.noreply.github.com>
2023-01-30 12:06:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let personagem = document.querySelector('#personagem')
+let quadrado = document.querySelector('#quadrado')
+
+function pular(){
+    if(personagem.classList != 'animar'){
+        personagem.classList.add('animar')
+    }
+
+    setTimeout(function(){
+        personagem.classList.remove('animar')
+    }, 500)
+}
+
+var testarColisao = setInterval( function){
+
+    var topoPersonagem = parseInt(
+        window.getComputedStyle(personagem).getPropertyValue('top')
+    )
+
+},10)
